cef499531cb17117811b617a57968e068fdf80c6
Santhosh Kumar <66013607+vaddepellis@users.noreply.github.com>
2022-09-09 13:09:14 +0530

    prod

--- web.js
+++ web.js
@@ -1,15 +1,14 @@
 const http = require('http');
-
-const hostname = 'https://sk-nodeapp.herokuapp.com';
-const port = 5000;
-
+const hostname =  'https://sk-nodeapp.herokuapp.com';
+const PORT = process.env.PORT || 5000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World!\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}`);
-//   console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.listen(PORT,hostname, () => console.log(`Listening on ${hostname}:${ PORT }`))
+//listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}`);
+// //   console.log(`Server running at http://${hostname}:${port}/`);
+// });
