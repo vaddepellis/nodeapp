e060a3d01254d64688e118d06e50194efa7d2d22
Santhosh Kumar <66013607+vaddepellis@users.noreply.github.com>
2022-09-30 10:25:20 +0530

    web socket initial commit

--- web.js
+++ web.js
@@ -1,23 +1,149 @@
-// Importing express module
-const express = require('express');
-const app = express();
+const Socket = require("websocket").server
+const http = require("http")
 
-// Getting Request
-app.get('/', (req, res) => {
 
-	// Sending the response
-	res.send('Hello World!')
+const server = http.createServer((req, res) => {})
+
+server.listen(3000, () => {
+    console.log("Listening on port 3000...")
+})
+
+const webSocket = new Socket({ httpServer: server })
+
+let users = []
+
+webSocket.on('request', (req) => {
+    const connection = req.accept()
+
+    connection.on('message', (message) => {
+        const data = JSON.parse(message.utf8Data)
+
+        const user = findUser(data.username)
+
+        switch(data.type) {
+            case "store_user":
+
+                if (user != null) {
+                    return
+                }
+
+                const newUser = {
+                     conn: connection,
+                     username: data.username
+                }
+
+                users.push(newUser)
+                console.log(newUser.username)
+                break
+            case "store_offer":
+                if (user == null)
+                    return
+                user.offer = data.offer
+                break
+            
+            case "store_candidate":
+                if (user == null) {
+                    return
+                }
+                if (user.candidates == null)
+                    user.candidates = []
+                
+                user.candidates.push(data.candidate)
+                break
+            case "send_answer":
+                if (user == null) {
+                    return
+                }
+
+                sendData({
+                    type: "answer",
+                    answer: data.answer
+                }, user.conn)
+                break
+            case "send_candidate":
+                if (user == null) {
+                    return
+                }
+
+                sendData({
+                    type: "candidate",
+                    candidate: data.candidate
+                }, user.conn)
+                break
+            case "join_call":
+                if (user == null) {
+                    return
+                }
+
+                sendData({
+                    type: "offer",
+                    offer: user.offer
+                }, connection)
+                
+                user.candidates.forEach(candidate => {
+                    sendData({
+                        type: "candidate",
+                        candidate: candidate
+                    }, connection)
+                })
+
+                break
+        }
+    })
+
+    connection.on('close', (reason, description) => {
+        users.forEach(user => {
+            if (user.conn == connection) {
+                users.splice(users.indexOf(user), 1)
+                return
+            }
+        })
+    })
+})
+
+function sendData(data, conn) {
+    conn.send(JSON.stringify(data))
+}
+
+function findUser(username) {
+    for (let i = 0;i < users.length;i++) {
+        if (users[i].username == username)
+            return users[i]
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Importing express module
+// const express = require('express');
+// const app = express();
+
+// // Getting Request
+// app.get('/', (req, res) => {
+
+// 	// Sending the response
+// 	res.send('Hello World!')
 	
-	// Ending the response
-	res.end()
-})
-app.get('/home',(req,res) =>{
-  res.send('Welcome to home page')
-})
+// 	// Ending the response
+// 	res.end()
+// })
+// app.get('/home',(req,res) =>{
+//   res.send('Welcome to home page')
+// })
 
-// Establishing the port
-const PORT = process.env.PORT ||5000;
+// // Establishing the port
+// const PORT = process.env.PORT ||5000;
 
-// Executing the server on given port number
-app.listen(PORT, console.log(
-`Server started on port ${PORT}`));
+// // Executing the server on given port number
+// app.listen(PORT, console.log(
+// `Server started on port ${PORT}`));
