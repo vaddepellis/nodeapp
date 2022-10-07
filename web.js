7a0298cd6deaf682b7ece1fb189c6840d9c4dee7
Santhosh Kumar <66013607+vaddepellis@users.noreply.github.com>
2022-10-07 13:03:00 +0530

    sk-nodeapp.herokuapp.com

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 const Socket = require("websocket").server
-const http = require("https") 
+const http = require("http") 
 const expressip = require('express-ip');
 const server = http.createServer(app)
 app.use(expressip().getIpInfoMiddleware)
